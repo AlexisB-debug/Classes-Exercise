6cacc5d009ad1b42a55dab3ce57371279369d354
Alexis Boegner <alexisboegner@gmail.com>
2025-10-15 15:02:23 -0400

    Completed Classes-Exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var car1 = new Car();
+            car1.Make = "DeLorean";
+            car1.Model = "DMC-12";
+            car1.Year = 1981;
+            
+            Console.WriteLine($"The 'Back to the Future' time machine is a {car1.Year} {car1.Make} {car1.Model}");
         }
     }
 }
